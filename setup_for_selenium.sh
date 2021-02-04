411ed05243872b483b63f15b88d28c059917d6dd
simplex06 <ozkanbali.76@gmail.com>
2021-02-04 21:06:18 +0100

    setup for selenium shell

---
+++ setup_for_selenium.sh
@@ -0,0 +1,5 @@
+curl https://intoli.com/install-google-chrome.sh | bash
+google-chrome --version
+wget https://chromedriver.storage.googleapis.com/88.0.4324.96/chromedriver_linux64.zip
+sudo yum install unzip
+unzip chromedriver_linux64
